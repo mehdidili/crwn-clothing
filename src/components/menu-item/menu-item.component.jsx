377f9bd3d94720d47e5d6147a60a188db66639ec
Mehdi <delmaghani@gmail.com>
2020-02-29 17:00:30 +0330

    update menu-items to naviagte

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import '../menu-item/menu-item.styles.scss';
-export const MenuItem = ({title, imageUrl, size}) => (
 
-    <div  className={`${size} menu-item`}>
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
+    <div  className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div style={{
             backgroundImage: `url(${imageUrl})` 
         }} className='background-image'></div>        
         <div className='content'>
             <h1 className='title'>{title.toUpperCase()}</h1>
-            <span className='subtitle'>SHOP NOW</span>
+            <span className='subtitle'>SHOP NOW</span> 
         </div>
     </div>
 
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
