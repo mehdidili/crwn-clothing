694f29261f5f938eab260bafc65e2d112fd67231
Mehdi <delmaghani@gmail.com>
2020-03-01 12:57:32 +0330

    update shop page and style shop page

---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './collection-item.styles.scss';
+
+const CollectionItem = ({id, name, price, imageUrl}) => (
+    <div className='collection-item'>
+        <div 
+        className='image'
+        style={{
+            backgroundImage: `url('${imageUrl}')`
+        }}
+        >
+                    </div>
+            <div class='collection-footer'>
+                <span class='name'>{name}</span>
+                <span class='price'>{price}</span>
+            </div>
+
+
+    </div>
+
+);
+
+export default CollectionItem ;
