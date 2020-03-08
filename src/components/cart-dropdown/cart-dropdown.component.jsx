8752e526287d6f137368b6e45145c7393b846619
Mehdi <delmaghani@gmail.com>
2020-03-08 16:55:31 +0330

    cart item added

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './cart-dropdown.styles.scss';
 import CustomButton from '../custom-button/custom-button.component';
-const CartDropdown = () => {
+import CartItem from '../cart-item/cart-item.component';
+import {connect} from 'react-redux';
+
+const CartDropdown = ({cartItems}) => {
         return (
             <div className='cart-dropdown' >
                 <div className='cart-items'>
-
+                    {
+                        cartItems.map(item => <CartItem key={item.id} item={item}/>)
+                    }
+                    
                 </div>
                 <CustomButton inverted >GO TO CHECK OUT</CustomButton>
             </div>
@@ -13,4 +19,8 @@ const CartDropdown = () => {
 
 };
 
-export default CartDropdown;
+const mapStateToProp = state => ({
+    cartItems: state.cart.cartItems
+});
+
+export default connect(mapStateToProp)(CartDropdown);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,5 @@
 import CartActionTypes from './cart.types';
+import {addItemToCart} from './cart.utils';
 const INITIAL_STATE = {
     hidden: true,
     cartItems: [
@@ -14,10 +15,9 @@ const CartReducer = (state = INITIAL_STATE, action) => {
                 hidden: !state.hidden
             }
         case CartActionTypes.ADD_ITEM:
-            console.log(action.payload);
             return {
                 ...state,
-                cartItems: [...state.cartItems, action.payload]
+                cartItems: addItemToCart(state.cartItems, action.payload)
             }
         default:
             return state;
