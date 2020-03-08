b51718ced4e65c952fe08cdacb39a224677fbdc9
Mehdi <delmaghani@gmail.com>
2020-03-08 20:42:53 +0330

    cart item added

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,11 +1,11 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
     const existingCartItem = cartItems.find(item =>     
-            item.id == cartItemToAdd.id
+            item.id === cartItemToAdd.id
         );
 
     if(existingCartItem )
         return cartItems.map(item =>
-            item.id == cartItemToAdd.id ?
+            item.id === cartItemToAdd.id ?
                 {...item, quantity: item.quantity + 1}
             :
              item 
